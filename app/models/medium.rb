a6bf87d1380b91f177396cb76287745fc1d39185
Josh Ullman <joshullman@gmail.com>
2016-05-06 15:37:31 -0700

    add recommended_to and recommended_by methods to Medium model

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -4,30 +4,40 @@ class Medium < ActiveRecord::Base
 	belongs_to :show
 
 	def watched_count
-		Like.where(media_id: self.id).count || 0
+		Like.where(medium_id: self.id).count || 0
 	end
 
 	def liked_count
-		Like.where(media_id: self.id).where(value: 1).count || 0
+		Like.where(medium_id: self.id).where(value: 1).count || 0
 	end
 
 	def seen_count
-		Like.where(media_id: self.id).where(value: 0).count || 0
+		Like.where(medium_id: self.id).where(value: 0).count || 0
 	end
 
 	def disliked_count
-		Like.where(media_id: self.id).where(value: -1).count || 0
+		Like.where(medium_id: self.id).where(value: -1).count || 0
 	end
 
 	def recommended_count
-		Recommendation.where(media_id: self.id).count || 0
+		Recommendation.where(medium_id: self.id).count || 0
 	end
 
 	def recommended_to(user_id)
-		User.where(Recommendation.where(sender: user_id).sender)
+		recs = self.recommendations.where(sender: user_id)
+		users = []
+		recs.each do |rec|
+			users << User.find(rec.receiver)
+		end
+		users
 	end
 
 	def recommended_by(user_id)
-		Recommendation.where(receiver: user_id)
+		recs = self.recommendations.where(receiver: user_id)
+		users = []
+		recs.each do |rec|
+			users << User.find(rec.sender)
+		end
+		users
 	end
 end
