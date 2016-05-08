89d92f55f0554f7cc4286865660f27237142a44c
Josh Ullman <joshullman@gmail.com>
2016-05-08 13:35:11 -0700

    create movie controller

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,2 +1,25 @@
 class MovieController < ApplicationController
+	before_action :find_movie, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@movies = Movie.all
+	end
+
+	def show
+		
+	end
+
+	def create
+
+	end
+
+	private
+
+	def find_movie
+		@movie = Movie.find(session[:movie_id])
+	end
+
+	def movie_params
+		params.require(:movie).permit(:title, :year, :rated, :released, :runtime, :genre, :director, :writer, :actors, :plot, :poster, :media_type, :imdb_id, :points)
+	end
 end
