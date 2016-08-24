b8561476b140c5dba4998d5798ebfe6fc4a9b769
Josh Ullman <joshullman@gmail.com>
2016-08-23 21:43:16 -0700

    can't seem to get chartkick to do anything i want it to do

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,3 +1,12 @@
-# Chartkick.options[:html] = '<div style="font-size: 50px; color: #f9f9f9;"></div>'
+Chartkick.options = {
+	height: "300px",
+}
+
+Chartkick.options[:library] = {
+	color: "#f9f9f9",
+	fontName: "'Amatic SC', cursive"
+}
+
+# Chartkick.options[:html] = '<div "height: 300px; style=font-size: 50px; color: #f9f9f9;"></div>'
 
 # Chartkick.options[:content_for] = :charts_js
